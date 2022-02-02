3bac633714fadfd1d94728571931a380ad4ce881
alazzaris <alb.lazzaris@gmail.com>
2022-02-02 16:23:58 +0100

    Update union.c

--- Exam_Rank_02/union.c
+++ Exam_Rank_02/union.c
@@ -12,33 +12,30 @@
 
 #include <unistd.h>
 
-int	ft_not_seen_before(char *s, int max_pos, char c)
+int		check_double(char *s, int max_pos, char c)
 {
 	int	i;
 
 	i = -1;
-	while (i < max_pos)
-	{
+	while(++i < max_pos)
 		if (s[i] == c)
 			return (0);
-	}
 	return (1);
 }
 
-void	ft_union(char *str1, char *str2)
+void	ft_union(char *s1, char *s2)
 {
 	int	i;
 	int	j;
 
 	i = -1;
-	while (str1[++i])
-		if (ft_not_seen_before(str1, i, str1[i]))
-			write(1, &str1[i], 1);
+	while (s1[++i])
+		if (check_double(s1, i, s1[i]))
+			write(1, &s1[i], 1);
 	j = -1;
-	while (str2[++j])
-		if (ft_not_seen_before(str1, i, str1[i])
-			&& ft_not_seen_before(str2, j, str2[j]))
-			write(1, &str2[j], 1);
+	while (s2[++j])
+		if (check_double(s1, i, s2[j]) && check_double(s2, j, s2[j]))
+			write(1, &s2[j], 1);
 }
 
 int	main(int argc, char **argv)
