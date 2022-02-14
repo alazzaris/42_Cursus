54e5849f480298aff385fe34e1155fd073a5e1f5
Alberto Lazzaris <alb.lazzaris@gmail.com>
2022-02-14 21:33:36 +0100

    push_swap init.c

--- push_swap/init.c
+++ push_swap/init.c
@@ -6,13 +6,13 @@
 /*   By: alazzari <alazzari@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 22:48:08 by alazzari          #+#    #+#             */
-/*   Updated: 2022/02/14 21:19:05 by alazzari         ###   ########.fr       */
+/*   Updated: 2022/02/14 21:32:28 by alazzari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_index(t_structs *str, t_stack *a)
+void	ft_index(t_structs *s, t_stack *a)
 {
 	unsigned int	i;
 	unsigned int	j;
@@ -21,10 +21,38 @@ void	ft_index(t_structs *str, t_stack *a)
 
 	i = 0;
 	control = ft_calloc (a->size, sizeof(int));
+	if (!control)
+		ft_exit (s, "error: malloc error\n", -1);
+	while (i < a->size)
+	{
+		j = 0;
+		index = 0;
+		while (j < a->size)
+		{
+			if (a->stack[j] < a->stack[i])
+				index++;
+			j++;
+		}
+		control[i] = index;
+		i++;
+	}
+	free (a->stack);
+	a->stack = control;
 }
 
-int	ft_is_sort()
-{}
+int	ft_is_sort(t_stack a)
+{
+	unsigned int	i;
+
+	i = 1;
+	while (i < a.size)
+	{
+		if (a.stack[i - 1] >= a.stack[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int	ft_overflow()
 {}
