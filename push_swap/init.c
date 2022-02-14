9c626130800b3d68c555e3b151e69d964e008d21
Alberto Lazzaris <alb.lazzaris@gmail.com>
2022-02-14 18:53:39 +0100

    push_swap upgrade

--- push_swap/init.c
+++ push_swap/init.c
@@ -6,7 +6,7 @@
 /*   By: alazzari <alazzari@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 22:48:08 by alazzari          #+#    #+#             */
-/*   Updated: 2022/02/13 23:00:24 by alazzari         ###   ########.fr       */
+/*   Updated: 2022/02/14 18:53:10 by alazzari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ void	ft_index(t_structs *str, t_stack *a)
 	unsigned int	i;
 	unsigned int	j;
 	unsigned int	index;
-	int						*ctrl;
-
-	
+	int				*ctrl;
 }
 
 int ft_sort()
