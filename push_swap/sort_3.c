09d21023f75d7fecf4ebfe9886ca34edb457bf58
Alberto Lazzaris <alb.lazzaris@gmail.com>
2022-02-16 23:15:58 +0100

    push_swap

--- push_swap/sort_3.c
+++ push_swap/sort_3.c
@@ -6,10 +6,16 @@
 /*   By: alazzari <alazzari@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 23:00:36 by alazzari          #+#    #+#             */
-/*   Updated: 2022/02/16 23:06:33 by alazzari         ###   ########.fr       */
+/*   Updated: 2022/02/16 23:14:07 by alazzari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_chunk
+t_chunk ft_chunk_one(t_structs *s, t_chunk chunk)
+{
+    chunk.min = chunk.max;
+    if (!chunk.max)
+        chunk.max = s->size / 2;
+    else if (chunk.max < s->size - s->size / 32)
+}
